6e7f3726d181b4fad2eb3f1e5af3c93989fc1557
Abhishek <abhishekmaddheshiya632@gmail.com>
2024-10-27 13:36:47 +0530

    Test1

---
+++ Tests/Test 1 (Simple Tddo)/script.js
@@ -0,0 +1,9 @@
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+const ul = document.querySelector("ul");
+
+button.addEventListener("click", (e)=>{
+    e.preventDefault();
+    ul.innerHTML += `<li>${input.value}</li>`
+    input.value = "";
+})
