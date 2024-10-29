f550a1f450328c8de1b8f9d242d037c839d8093a
Abhishek <abhishekmaddheshiya632@gmail.com>
2024-10-29 10:34:18 +0530

    follow, menu

--- Menu Feature/script.js
+++ Menu Feature/script.js
@@ -1,15 +1,18 @@
-const nav = document.querySelector('nav');
-const menu = document.querySelector('.menu');
-const close = document.querySelector('.close');
+const menu = document.querySelector(".menu");
+const close = document.querySelector(".close");
+const nav = document.querySelector("nav");
+const ul = document.querySelector("ul");
 
-menu.addEventListener('click', () => {
+menu.addEventListener("click", () => {
     nav.style.height = "100%";
+    ul.style.display = "flex";
     menu.style.display = "none";
     close.style.display = "initial";
 });
 
-close.addEventListener('click', () => {
+close.addEventListener("click", () => {
     nav.style.height = "10%";
+    ul.style.display = "none";
     menu.style.display = "initial";
     close.style.display = "none";
-})
+});
